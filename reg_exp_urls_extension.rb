45df1aedabf6ba60fd086e034ad8d81cfa3cd339
Phil Burrows <peburrows@gmail.com>
2008-02-01 00:42:28 +0000

    just updating some documentation

--- reg_exp_urls_extension.rb
+++ reg_exp_urls_extension.rb
@@ -1,7 +1,7 @@
 class RegExpUrlsExtension < Radiant::Extension
   version "1.0"
-  description "Allows for urls of available pages to be found via a regular expression"
-  url "http://philburrows.com"
+  description "Allows for urls of available pages to be found via a regular expression. Very, very similar to &lt;r:find /&gt; tag."
+  url "http://dev.philburrows.com/svn/radiant-extensions/reg_exp_urls/trunk"
   
   def activate
     Page.send :include, FinderTags
